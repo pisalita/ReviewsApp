6cc750df2af2a97908f2a1cbd991660df1463754
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-11-25 22:02:34 +0100

    Showing form errors, with some real time validation

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -37,7 +37,9 @@ export default function ReviewForm({ addReview }) {
               placeholder='Review title'
               onChangeText={props.handleChange('title')}
               value={props.values.title}
+              onBlur={props.handleBlur('title')}
             />
+            <Text style={globalStyles.errorText}>{ props.touched.title && props.errors.title }</Text>
 
             <TextInput 
               multiline
@@ -45,7 +47,9 @@ export default function ReviewForm({ addReview }) {
               placeholder='Review body'
               onChangeText={props.handleChange('body')}
               value={props.values.body}
+              onBlur={props.handleBlur('body')}
             />
+            <Text style={globalStyles.errorText}>{ props.touched.body && props.errors.body }</Text>
 
             <TextInput 
               style={globalStyles.input}
@@ -53,7 +57,9 @@ export default function ReviewForm({ addReview }) {
               onChangeText={props.handleChange('rating')}
               value={props.values.rating}
               keyboardType='numeric'
+              onBlur={props.handleBlur('rating')}
             />
+            <Text style={globalStyles.errorText}>{ props.touched.rating && props.errors.rating }</Text>
 
             <Button title='submit' color='maroon' onPress={props.handleSubmit} />
           </View>
