6b6ad38c341975f1d64571f916c936d4a2d7b689
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-11-25 20:45:25 +0100

    Added a form with formik, so you can add a new review

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal,
+  TouchableWithoutFeedback, Keyboard, } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 import { MaterialIcons } from '@expo/vector-icons';
+import ReviewForm from './reviewForm';
 
 export default function Home({ navigation }) {
 
@@ -13,19 +15,29 @@ export default function Home({ navigation }) {
     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
   ]);
 
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    setReviews((currentReviews) => {
+      return [review, ...currentReviews]
+    });
+    setModalOpen(false);
+  }
+
   return (
     <View style={globalStyles.container}>
 
       <Modal visible={modalOpen} animationType='slide'> 
-        <View styles={styles.modalContent}>
-          <MaterialIcons 
-            name='close'
-            size={24}
-            style={{ ...styles.modalToggle, ...styles.modalClose}}
-            onPress={() => setModalOpen(false)}
-          />
-          <Text>Hello from the modal :)</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <MaterialIcons 
+              name='close'
+              size={24}
+              style={{ ...styles.modalToggle, ...styles.modalClose}}
+              onPress={() => setModalOpen(false)}
+            />
+            <ReviewForm addReview={addReview}/>
+          </View>
+        </TouchableWithoutFeedback>
       </Modal>
 
       <MaterialIcons 
